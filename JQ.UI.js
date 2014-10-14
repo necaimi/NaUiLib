84114e5092fce95eed3acd11a960d4025410a886
necaimi <308048801@qq.com>
2014-10-14 12:10:07 +0800

    2014-10-14

--- JQ.UI.js
+++ JQ.UI.js
@@ -1,17 +1,22 @@
 
     
-    
-    function checkedHandler(event, state){};
+var Fn = Function, global = (new Fn("return this"))();
+    if(!global.UiLib){
+        global.UiLib = {};
+    }
 
-    var checkedHandler_fn = checkedHandler;
-    
-
-    function SelectBox(obj){
+    global.UiLib.SelectBox = {
+        checkedHandler : function(event, state){
+        },
+        
+        Initlize : function(){
+            
+        function SelectBox(obj){
         this._obj = obj,
-        this._oldIndex = -1;
+        this._oldIndex = null;
         this._itemmap = {};
         this._isMutux = true;
-    };
+        };
     
     SelectBox.prototype.getObj = function(){
         return this._obj;
@@ -20,46 +25,28 @@
     SelectBox.prototype.setMutux = function(b){
         this._isMutux = b;
     };
-
-    SelectBox.prototype.getItems = function(){
-        if(this._isMutux){
-        return this._itemmap["item-"+this._oldIndex];
-        }else{
-        var _items = [], i = 0, len = this._itemmap.length;
-            for(i; i < len; i++){
-                var curItem = this._itemmap["item-"+i];
-                if( curItem._checked)
-                {
-                   _items.push(curItem);
-                }
-            }
-            
-            return _items;
-            
-        }
-    };
     
-    SelectBox.prototype.addItem = function(obj, index, cfn){
-        
-        if(!obj && index != null){
+    SelectBox.prototype.addItem = function(obj, cfn, index){
+
+        if(!obj){
             return null;
         }
         
         if(this._itemmap["item-"+index]){
             return null;
         }
-        
-        var _item = new Item(obj, index, cfn, this);
+      
+        var _item = new Item(obj, cfn, index, this);
+       
         $(obj).bind("click", function(){
-            var isMutux = _item._parent._isMutux;
-            if(_item._checked && isMutux){
+            var _parent = _item._parent;
+            if(_item._checked &&  _parent._isMutux)
                 return true;
-            }
             
-            if(!isMutux){
-            _item._checked ? _item._parent._cancelItem(_item) : _item._parent._selectItem(_item);
+            if(_parent._isMutux){
+            _parent._selectItem(_item);
             }else{
-            _item._parent._selectItem(_item);
+            _item._checked ? _parent._cancelItem(_item) : _parent._selectItem(_item);
             }
             
         }).bind("checked", function(){
@@ -79,27 +66,48 @@
     SelectBox.prototype._selectItem = function(obj){
         obj.check(true);
         
-        if(this._oldIndex != -1 && this._isMutux){
+        if(this._oldIndex != null && this._isMutux){
         this._itemmap["item-"+this._oldIndex].check(false);
         }
         
         this._oldIndex = obj._index;
     };
     
-     SelectBox.prototype.selectIndex = function(index){
-         if(this._itemmap["item-"+index])
-         {
-            this._selectItem(this._itemmap["item-"+index]);
-         }
-
-         
+    SelectBox.prototype.selectIndex = function(index){
+           var curObj =  this._itemmap["item-"+index];
+           this._selectItem(curObj);
+            
      };
-    
+        
+    SelectBox.prototype.cancelInex = function(index){
+          var curObj =  this._itemmap["item-"+index];
+           this._cancelItem(curObj);
+    };
+            
     SelectBox.prototype._cancelItem = function(obj){
-        this._itemmap[obj].check(false);
+            obj.check(false);
+    };
+            
+    SelectBox.prototype.getItems = function(){
+        var retItem = [];
+        if(this._isMutux){
+            retItem.push(this._itemmap["item-"+this._oldIndex]);
+            return retItem;
+        }else{
+            var i = 0, len = this._itemmap.length;
+            for(i; i < len ; i++){
+                var curItem = this._itemmap["item-"+i];
+                if(curItem.checked){
+                    retItem.push(curItem);
+                }
+            }
+            
+            return curItem;
+            
+        }
     };
     
-    function Item(obj, index, cfn, parent){
+    function Item(obj, cfn, index, parent){
         this._obj = obj,
         this._cfn = cfn,
         this._checked = false,
@@ -120,14 +128,23 @@
  $(document).ready(function(){
      $("[c-type='selectbox']").each(function(index, element){
          var _selectBox = new SelectBox(this), $selbox = $(this);
-       
+      
          $selbox.find("[c-type='item']").each(function(index, element){
-
-             _selectBox.addItem(this, index, checkedHandler_fn);
+             _selectBox.addItem(this, UiLib.SelectBox.checkedHandler, index);
          });
+         
          this.prototype = _selectBox;
          
-         var defWparam = $selbox.attr("def-num");
+         var defWparam = $selbox.attr("def-select");
          _selectBox.selectIndex(defWparam);
      });
     });
+
+        }
+    };
+
+    UiLib.SelectBox.Initlize();
+
+   
+    
+   
